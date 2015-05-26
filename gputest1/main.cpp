c850cbbbe2f91107bbe53dbfae17f4323ee06126
nicholac <kris_nich@hotmail.co.uk>
2015-05-26 19:09:46 +0100

    Initial Commit

---
+++ gputest1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gputest1
+//
+//  Created by Chris Nicholas on 26/05/2015.
+//  Copyright (c) 2015 Chris Nicholas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
